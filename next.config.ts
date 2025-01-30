43e9b20e63795bef11f82c8684ca86cac2c273ff
Kristo Kiis <kristo.kiis@hear.com>
2025-01-29 22:45:00 -0500

    Made custom domain to work

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,7 @@ const nextConfig: NextConfig = {
   output: 'export',  // This tells Next.js to export static files
   images: {
     unoptimized: true,
-  },
-  basePath: '/howdoyoufindme-frontend'  // Should match your repository name
+  }
 };
 
 export default nextConfig;
