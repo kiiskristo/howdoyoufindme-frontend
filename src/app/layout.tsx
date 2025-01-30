82ffaa065b417993605c6b641c584979b54c9a39
Kristo Kiis <kristo.kiis@hear.com>
2025-01-29 23:03:03 -0500

    Added Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
+import GoogleAnalytics from '@/components/GoogleAnalytics'
 
 export const metadata: Metadata = {
   title: 'HowDoYouFindMe',
@@ -13,7 +14,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <GoogleAnalytics ga_id={process.env.NEXT_PUBLIC_GA_ID!} />
+        {children}
+        </body>
     </html>
   )
 }
